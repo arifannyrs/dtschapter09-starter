a4b5d91e38d2e1e4f0cca0d7650180ec9fab2acf
arifannyrs <arifannyrs@gmail.com>
2019-11-25 16:09:54 +0700

    Update Praktikum 6

--- DTSApp/app/src/main/java/polinema/ac/id/dtsapp/RegisterActivity.java
+++ DTSApp/app/src/main/java/polinema/ac/id/dtsapp/RegisterActivity.java
@@ -1,6 +1,8 @@
 package polinema.ac.id.dtsapp;
 
 import android.app.ProgressDialog;
+import android.arch.persistence.room.RoomDatabase;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import polinema.ac.id.dtsapp.data.AppDbProvider;
+import polinema.ac.id.dtsapp.data.DTSAppDatabase;
+import polinema.ac.id.dtsapp.data.DatabaseTask;
+import polinema.ac.id.dtsapp.data.DatabaseTaskEventListener;
 import polinema.ac.id.dtsapp.data.User;
 import polinema.ac.id.dtsapp.data.UserDao;
 
@@ -37,19 +42,54 @@ public class RegisterActivity extends AppCompatActivity
         this.edtPhoneNumber = this.findViewById(R.id.edt_phone_number);
     }
 
+//    public void onBtnRegisterNow_Click_old(View view)
+//    {
+//        // Mendapatkan DAO dari DTSAppDatabase
+//        UserDao daoUser = AppDbProvider.getInstance(this.getApplicationContext()).userDao();
+//
+//        // Menggunakan DAO untuk melakukan INSERT data dalam objek dari class Entity User
+//        daoUser.insertAll(this.makeUser());
+//
+//        // Tampilkan pesan konfirmasi
+//        Toast.makeText(this, "Register Success!", Toast.LENGTH_SHORT).show();
+//
+//        // Kembali ke halaman login
+//        this.finish();
+//    }
+
     public void onBtnRegisterNow_Click(View view)
     {
-        // Mendapatkan DAO dari DTSAppDatabase
-        UserDao daoUser = AppDbProvider.getInstance(this.getApplicationContext()).userDao();
+        // Tampilkan loading indicator
+        this.showLoadingIndicator();
 
-        // Menggunakan DAO untuk melakukan INSERT data dalam objek dari class Entity User
-        daoUser.insertAll(this.makeUser());
+        new DatabaseTask(this, new DatabaseTaskEventListener() {
 
-        // Tampilkan pesan konfirmasi
-        Toast.makeText(this, "Register Success!", Toast.LENGTH_SHORT).show();
+            @Override
+            public Object runDatabaseOperation(RoomDatabase database, Object... params) {
 
-        // Kembali ke halaman login
-        this.finish();
+                // Mengambil Entity dari params
+                User user = (User) params[0];
+
+                // Mendapatkan DAO dari object database, dan memanggil method operasi INSERT
+                ((DTSAppDatabase)database).userDao().insertAll(user);
+
+                return null;
+            }
+
+            @Override
+            public void onDatabaseOperationFinished(Object... results)
+            {
+                // Delay eksekusi program agar nampak agak lama seolah-olah datanya sedang diunggah
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        // Tutup loading indicator & tampilkan Toast
+                        loadingIndicator.dismiss();
+                        Toast.makeText(getApplicationContext(), "Registration success!", Toast.LENGTH_SHORT).show();
+                    }
+                }, 5000);
+            }
+        }).execute(this.makeUser());
     }
 
     private void showLoadingIndicator()
